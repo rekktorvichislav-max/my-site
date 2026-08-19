f12c7a037ca3e8bad1e697901155b9f3c89f5ce7
rekktorvichislav-max <rekktorvichislav-max@github.com>
2026-08-19 12:00:27 +0300

    New Axion Client mod icon, update Telegram link to t.me/Axion_Client

--- lib/social.ts
+++ lib/social.ts
@@ -1,4 +1,4 @@
 export const SOCIAL = {
-  telegram: process.env.NEXT_PUBLIC_SOCIAL_TG || "https://t.me/Cotax_Client",
+  telegram: process.env.NEXT_PUBLIC_SOCIAL_TG || "https://t.me/Axion_Client",
   discord: process.env.NEXT_PUBLIC_SOCIAL_DS || "https://discord.gg/WYaeGWs9R",
 };
